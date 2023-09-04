29a47ca198108e294fd4e80c296190a17117d605
João Ferreira <joaovtf39@gmail.com>
2023-09-03 22:53:51 -0300

    chore: add gorm and postgresql driver

--- main.go
+++ main.go
@@ -7,13 +7,20 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
+type FIICard struct {
+	Title string `json:"title"`
+}
+
 func main() {
 	c := colly.NewCollector()
 	c.IgnoreRobotsTxt = true
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+	var fiiCards []FIICard;
+	// brazilianReciptSuffix := "B"
 
-	c.OnHTML("", func(h *colly.HTMLElement) {
-		fmt.Println("Texto do botão:", h.Text)
+	c.OnHTML(".tickerBox__title", func(h *colly.HTMLElement) {
+		fmt.Println("Texto:", h.Text)
+		fiiCards = append(fiiCards, FIICard{Title: h.Text})
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -26,14 +33,13 @@ func main() {
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Page visited: ", r.Request.URL)
-		body := string(r.Body)
-		fmt.Println("Body:", body)
+		// fmt.Println("Page visited: ", r.Request.URL)
+		// body := string(r.Body)
+		// fmt.Println("Body:", body)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println(r.Request.URL, "scraped!")
 	})
-
-	c.Visit("https://www.fundsexplorer.com.br/")
+	c.Visit("https://www.fundsexplorer.com.br/funds")
 } 
