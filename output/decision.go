efd16092a0cb6834df6aab50e275869db3180809
João Ferreira <joaovtf39@gmail.com>
2023-09-10 19:54:53 -0300

    refactor: extract logic to separeted methods

--- output/decision.go
+++ output/decision.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -33,19 +34,15 @@ func Decision() {
 
 	for _, fund := range funds {
 		r := Result{}
-
 		dy, err := formatPercentage(fund.DividendYield); if err != nil {
 			fmt.Println(err)
 		}
-
 		pvp, err := formatPvp(fund.Pvp); if err != nil {
 			fmt.Println(err)
 		} 
-
 		liq, err := parseStringToFloat(fund.Liquidity); if err != nil {
 			fmt.Println(err)
 		}
-
 		r.Code = fund.Code
 		r.ResultDetail.DividendYield = dy
 		r.ResultDetail.Liquidity = liq
@@ -56,14 +53,28 @@ func Decision() {
 		resultRaw = append(resultRaw, r)
 	}
 
-	resultJSON, _ := json.MarshalIndent(resultRaw, "", "  ")
-	os.WriteFile("result.json", resultJSON, 0666) 
+	orderFunds(resultRaw)
+	filteredResults := filterFundsAboveCriteria(resultRaw, 15)
+	bestFunds := selectBestFunds(filteredResults, 10)
+	resultJSON, _ := json.MarshalIndent(bestFunds, "", "  ")
+	os.WriteFile("chosen_funds.json", resultJSON, 0666)
 }
 
 func formatPercentage(input string) (float64, error) {
 	input = strings.ReplaceAll(input, " ", "")
 	input = strings.Replace(input, ",", ".", 1)
 	input = strings.TrimRight(input, "%")
+	value, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0.0, err
+	}
+	return value, nil
+}
+
+func formatPvp(input string) (float64, error) {
+	input = strings.TrimSpace(input)
+	input = strings.TrimRight(input, " \t\n\r")
+	input = strings.Replace(input, ",", ".", 1)
 	value, err := strconv.ParseFloat(input, 64)
 	if err != nil {
 		return 0.0, err
@@ -87,13 +98,38 @@ func parseStringToFloat(input string) (float64, error) {
 	return value, nil
 }
 
-func formatPvp(input string) (float64, error) {
-	input = strings.TrimSpace(input)
-	input = strings.TrimRight(input, " \t\n\r")
-	input = strings.Replace(input, ",", ".", 1)
-	value, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0.0, err
+func orderFunds(results []Result) {
+	sort.Slice(results, func(i, j int) bool {
+		if results[i].ResultDetail.DividendYield > results[j].ResultDetail.DividendYield {
+			return true
+		} else if results[i].ResultDetail.DividendYield < results[j].ResultDetail.DividendYield {
+			return false
+		}
+		if results[i].ResultDetail.Liquidity > results[j].ResultDetail.Liquidity {
+			return true
+		} else if results[i].ResultDetail.Liquidity < results[j].ResultDetail.Liquidity {
+			return false
+		}
+		return results[i].ResultDetail.Pvp < results[j].ResultDetail.Pvp
+	})
+}
+
+func filterFundsAboveCriteria(resultRaw []Result, maxDividendYield float64) []Result {
+	filteredResults := make([]Result, 0)
+	for _, r := range resultRaw {
+		if r.ResultDetail.DividendYield <= maxDividendYield {
+			filteredResults = append(filteredResults, r)
+		}
 	}
-	return value, nil
+	return filteredResults
+}
+
+func selectBestFunds(filteredResults []Result, maxCount int) []Result {
+	var bestFunds []Result
+	if len(filteredResults) >= maxCount {
+		bestFunds = filteredResults[:maxCount]
+	} else {
+		bestFunds = filteredResults
+	}
+	return bestFunds
 }
