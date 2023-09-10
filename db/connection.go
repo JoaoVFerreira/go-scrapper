d84cd2b054e91edfe3109507b81b96bf975f5a2b
João Ferreira <joaovtf39@gmail.com>
2023-09-09 23:38:20 -0300

    feat: add structs method for realEstate db

--- db/connection.go
+++ db/connection.go
@@ -18,15 +18,17 @@ var DB *gorm.DB
 
 type RealStateFund struct {
 	gorm.Model
-	ID          uuid.UUID 		 `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
-	Code        string         `gorm:"size:255;not null;unique;index" json:"code"`
-	Type        string         `gorm:"size:255;not null;" json:"type"`
-	Description string         `gorm:"size:255;not null;" json:"description"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at,omitempty"`
+	ID           	uuid.UUID 			`gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
+	Code         	string    			`gorm:"size:255;not null;unique;index" json:"code"`
+	Type         	string    			`gorm:"size:255;not null;" json:"type"`
+	Description  	string    			`gorm:"size:255;not null;" json:"description"`
+	DividendYield string   				`gorm:"size:255;" json:"dividend_yield"`
+	Pvp          	string    			`gorm:"size:255;" json:"pvp"`
+	Liquidity    	string    			`gorm:"size:255;" json:"liquidity"`
+	CreatedAt    	time.Time 			`json:"created_at"`
+	UpdatedAt    	time.Time 			`json:"updated_at"`
+	DeletedAt    	gorm.DeletedAt  `json:"deleted_at,omitempty"`
 }
-
 
 func (f *RealStateFund) SaveFund() (*RealStateFund, error) {
 	err := DB.Create(&f).Error; if err != nil {
@@ -52,6 +54,61 @@ func (f *RealStateFund) BeforeSave(tx *gorm.DB) error {
 	return nil
 }
 
+func (f *RealStateFund) GetAll() ([]*RealStateFund, error) {
+	funds := []*RealStateFund{}
+	err := DB.Find(&funds).Error; if err != nil {
+		return nil, err
+	} 
+	return funds, nil
+}
+
+func (f *RealStateFund) UpdateFund(dy, pvp, liquidity string) (error) {
+	err := DB.Model(f).Where("code = ?", f.Code).Updates(map[string]interface{}{
+		"dividend_yield": dy,
+		"pvp":            pvp,
+		"liquidity":      liquidity,
+	}).Error; if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *RealStateFund) FindByCode(code string) (*RealStateFund, error) {
+	var fund RealStateFund
+	err := DB.Where("code = ?", code).First(&fund).Error
+	if err != nil {
+		return nil, err 
+	}
+	return &fund, nil
+}
+
+func (f *RealStateFund) BeforeUpdate(tx *gorm.DB) error {
+	if f.DividendYield == "" || f.Pvp == "" || f.Liquidity == "" {
+		return errors.New("All fields must be filled in.")
+	}
+
+	dy := strings.ReplaceAll(f.DividendYield, " ", "")
+	pvp := strings.ReplaceAll(f.Pvp, " ", "")
+	l := strings.ReplaceAll(f.Liquidity, " ", "")
+
+	if strings.Contains(dy, "N/A") || strings.Contains(pvp, "N/A" ) || strings.Contains(l, "N/A" ) {
+		return errors.New("Field is not allowed to contain 'N/A'.")
+	}
+
+	f.DividendYield = dy
+	f.Liquidity = l
+	f.Pvp = pvp
+	return nil
+}
+
+func (f *RealStateFund) FindAllWithData() ([]RealStateFund, error) {
+	var funds []RealStateFund
+	err := DB.Where("pvp IS NOT NULL AND liquidity IS NOT NULL AND dividend_yield IS NOT NULL").Find(&funds).Error
+	if err != nil {
+		return nil, err 
+	}
+	return funds, nil 
+}
 
 func ConnectToDataBase() {
   err := godotenv.Load(".env"); if err != nil {
